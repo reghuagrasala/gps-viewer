3e66e606042ade1053ee862d2ceb45d497bf2407
reghuagrasala <87054723+reghuagrasala@users.noreply.github.com>
2026-09-13 21:52:29 +0530

    Bump service worker for GPS Viewer v24

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gps-viewer-v23";
-const CORE=["./","./index.html","./style.css","./app.js","./v23-fixes.js","./config.js","./digipin.js","./offline-cache.js","./manifest.webmanifest","./favicon.svg","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
+const CACHE="gps-viewer-v24";
+const CORE=["./","./index.html","./style.css","./app.js","./config.js","./digipin.js","./offline-cache.js","./manifest.webmanifest","./favicon.svg","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
