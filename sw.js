9f9fd47526fc70330e899668bad357e1d8062099
reghuagrasala <87054723+reghuagrasala@users.noreply.github.com>
2026-09-13 21:04:42 +0530

    Bump service worker to v22 and remove faulty v20 cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gps-viewer-v21";
-const CORE=["./","./index.html","./style.css","./app.js","./v17-fixes.js","./v18-fixes.js","./v19-fixes.js","./v20-fixes.js","./v21-fixes.js","./v15-fixes.js","./v16-fixes.js","./config.js","./digipin.js","./offline-cache.js","./manifest.webmanifest","./favicon.svg","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
+const CACHE="gps-viewer-v22";
+const CORE=["./","./index.html","./style.css","./app.js","./v17-fixes.js","./v18-fixes.js","./v19-fixes.js","./v21-fixes.js","./v15-fixes.js","./v16-fixes.js","./config.js","./digipin.js","./offline-cache.js","./manifest.webmanifest","./favicon.svg","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
